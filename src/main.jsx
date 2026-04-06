dfc6de8b7514735bf55669853bf7b44436785ba5
Nimish Gupta <nimish29092006@gmail.com>
2026-04-06 15:05:10 +0530

    Describe what you changed

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../styles.css'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#05070d',
+          color: '#fff',
+          fontFamily: 'monospace'
+        }}>
+          Something broke. Reload.
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
